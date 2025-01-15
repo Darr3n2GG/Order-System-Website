1cb07913dee5f2105ec9589694b89d68cb393749
Darr3n <darrensoonyuchien@gmail.com>
2025-01-15 11:44:03 +0800

    added category dialog

---
+++ src/frontend/menu/menu.js
@@ -0,0 +1,4 @@
+const categoryButton = document.getElementById("category-button")
+const categoryDialog = document.getElementById("category-dialog")
+
+categoryButton.addEventListener('click', () => categoryDialog.show());
