a20244b49621b37188d1c224b8a5f4eaa9b3c335
Darr3n <darrensoonyuchien@gmail.com>
2025-02-16 17:52:03 +0800

    added cart ui mock

--- src/frontend/menu/cart.js
+++ src/frontend/menu/cart.js
@@ -1,11 +1,23 @@
 const cart = [];
 
-const add_item_button = document.querySelector(".add_item_button");
+const addItemButton = document.querySelector(".add_item_button");
+const cartButton = document.querySelector(".cart_button");
+const cartDiv = document.querySelector(".cart");
 
-add_item_button.addEventListener("click", () => {
-    addItemToCart(add_item_button.value);
+addItemButton.addEventListener("click", () => {
+    addItemToCart(addItemButton.value);
 });
+
+cartButton.addEventListener("click", () => {
+    const cartDialog = cartDiv.querySelector(".cart_dialog");
+    cartDialog.show();
+})
 
 function addItemToCart(item) {
     cart.push(item);
+    const itemList = cartDiv.querySelector(".item_list");
+    const itemElement = document.createElement("li");
+    itemElement.innerHTML = `${cart.label + cart.nombor} ${cart.nama} : ${cart.quantiti}`;
+    itemList.appendChild(itemElement);
 }
+
