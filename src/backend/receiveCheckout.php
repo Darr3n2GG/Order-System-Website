d2c8982163f1e94b9b2badf9a03a7d867869def6
Darr3n <darrensoonyuchien@gmail.com>
2025-03-01 14:56:51 +0800

    bug fix in receiveCheckout.php

--- src/backend/receiveCheckout.php
+++ src/backend/receiveCheckout.php
@@ -3,9 +3,9 @@ include_once("account.php");
 $user_id = getUserIDFromSession();
 
 $cartStringData = $_POST["cart"];
-$cartJSONData = json_decode($checkoutStringData);
+$cartJSONData = json_decode($cartStringData);
 
 // Create pesanan column with N belians
 
 
-echo json_encode($checkoutJSONData);
+echo json_encode($cartJSONData);
