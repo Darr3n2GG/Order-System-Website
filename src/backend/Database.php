3d43dd06c8c5b65a4c3b30a828850ba80ebaa3cf
Darr3n <darrensoonyuchien@gmail.com>
2025-03-19 17:38:49 +0800

    Update Database.php

--- src/backend/Database.php
+++ src/backend/Database.php
@@ -1,7 +1,7 @@
 <?php
 require_once(__DIR__ . "/MySQLConnector.php");
 
-date_default_timezone_set("Asia/Penang");
+date_default_timezone_set("Asia/Kuala_Lumpur");
 
 function DatabaseFactory(): MySQLConnector {
     $HOST = "localhost";
